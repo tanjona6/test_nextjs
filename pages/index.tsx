b8f1266da93ca76acd137a7ade2940db06f35163
TANJONIAINA Danie <104355549+tanjona6@users.noreply.github.com>
2025-04-30 21:56:34 +0300

    second commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -15,28 +15,28 @@ export const getStaticProps: GetStaticProps<{ articles: Article[] }> = async () 
 
 export default function Home({ articles }: InferGetStaticPropsType<typeof getStaticProps>) {
   return (
-    <main className="p-6 max-w-4xl mx-auto">
-    <h1 className="text-3xl font-bold mb-8 text-center">Liste des articles</h1>
-    
-    <div className="grid grid-cols-1 sm:grid-cols-2 gap-6">
-      {articles.map((article) => (
-        <div key={article.id} className="border p-4 rounded shadow hover:shadow-lg transition">
-          <Link href={`/articles/${article.id}`}>
-            <h2 className="text-xl text-blue-600 hover:underline">{article.title}</h2>
-          </Link>
-          <Image
-            src={article.image}
-            alt={article.title}
-            width={150}
-            height={100}
-            className="my-2 w-auto max-w-full object-cover rounded"
-          />
-          <p className="text-gray-700">{article.description}</p>
-        </div>
-      ))}
-    </div>
-  </main>
+  <main className="p-6 max-w-4xl mx-auto">
+  <h1 className="text-3xl font-bold mb-8 text-center">Liste des articles</h1>
   
+  <div className="grid grid-cols-1 sm:grid-cols-2 gap-6">
+    {articles.map((article) => (
+      <div key={article.id} className="border p-4 rounded shadow hover:shadow-lg transition">
+        <Link href={`/articles/${article.id}`}>
+          <h2 className="text-xl text-blue-600 hover:underline">{article.title}</h2>
+        </Link>
+        <Image
+          src={article.image}
+          alt={article.title}
+          width={150}
+          height={100}
+          className="my-2 w-auto max-w-full object-cover rounded"
+        />
+        <p className="text-gray-700">{article.description}</p>
+      </div>
+    ))}
+  </div>
+</main>
+
   
 
   );
